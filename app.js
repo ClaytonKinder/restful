11fcbe5d81fb7f3bf8cb87bf115c323cd7808151
Clayton Kinder <Clayton@gaugeinteractive.com>
2016-05-17 16:58:39 -0400

    Changed URL for allowed access.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.bodyParser());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://kraftee.dev');
+    res.setHeader('Access-Control-Allow-Origin', 'https://claytonkinder.github.io');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
