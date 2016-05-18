406921234ed9ef38fa1fff20a81a257939d07a38
Clayton Kinder <Clayton@gaugeinteractive.com>
2016-05-18 09:52:46 -0400

    Updated body-parser.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
 
